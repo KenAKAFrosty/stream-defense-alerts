794713a57e91492df57084e08cd39a8c1adbed61
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-07-09 18:03:58 -0400

    better failure communication, await bodyparse

--- src/routes/webhooks/index.ts
+++ src/routes/webhooks/index.ts
@@ -2,13 +2,17 @@ import { type RequestHandler } from "@builder.io/qwik-city";
 import { getQueryBuilder } from "~/database/functions";
 
 export const onRequest: RequestHandler = async (event) => {
+    const body = await event.parseBody();
+    if (body === null) { 
+        event.json(500, {value: null, failure: "No body provided. Please provide a JSON body and be sure to include the 'Content-Type' header of 'application/json'"})
+        return;
+    }
     const method = event.request.method;
     const headers = event.request.headers.entries();
-    const body = event.parseBody();
     console.log({ method, headers, body })
     const qb = getQueryBuilder();
     await qb.insertInto("incoming_webhooks").values({
         payload: JSON.stringify({ method, headers, body }),
     }).execute();
-    event.json(200, { value: "received and processed", failure: null });
+    event.json(200, { value: "Received and processed", failure: null });
 }
