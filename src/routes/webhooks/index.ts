ec9e36304f8c9e7a4f7f1637d9c785d0e27eb6fa
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-07-09 17:59:32 -0400

    fix request targeting

--- src/routes/webhooks/index.ts
+++ src/routes/webhooks/index.ts
@@ -1,13 +1,14 @@
 import { type RequestHandler } from "@builder.io/qwik-city";
 import { getQueryBuilder } from "~/database/functions";
 
-export const onGet: RequestHandler = async (event) => {
+export const onRequest: RequestHandler = async (event) => {
+    const method = event.request.method;
     const headers = event.request.headers.entries();
     const body = event.parseBody();
     console.log(headers);
     console.log(body);
     const qb = getQueryBuilder();
     qb.insertInto("incoming_webhooks").values({ 
-        payload: JSON.stringify({headers, body}),
+        payload: JSON.stringify({method, headers, body}),
     })
 }
