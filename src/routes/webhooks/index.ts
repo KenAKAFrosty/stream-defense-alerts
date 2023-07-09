fca24b3a9b7803132b4394e51ecb8dde167317a4
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-07-09 18:06:00 -0400

    restrict to post request

--- src/routes/webhooks/index.ts
+++ src/routes/webhooks/index.ts
@@ -1,7 +1,7 @@
 import { type RequestHandler } from "@builder.io/qwik-city";
 import { getQueryBuilder } from "~/database/functions";
 
-export const onRequest: RequestHandler = async (event) => {
+export const onPost: RequestHandler = async (event) => {
     const body = await event.parseBody();
     if (body === null) { 
         event.json(500, {value: null, failure: "No body provided. Please provide a JSON body and be sure to include the 'Content-Type' header of 'application/json'"})
